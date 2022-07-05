1460e7604a3dcacb9404308c5ba66e2a866f3e80
unlessprod <103318610+unlessprod@users.noreply.github.com>
2022-07-05 12:23:14 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,9 +11,9 @@ let config = {
 
   // qq
   social: {
-    telegram: '@ddguard',
-    instagram: 'ddguard',
-    vk: 'ddguard',
+    telegram: '@fullshow',
+    instagram: 'fullshow',
+    vk: 'fullshow',
   },
 
 
